c4fe401aa1b6c3cb019737c96cc4c2a720e9ac89
Vivek Nayyar <vivek.of.nayyar@gmail.com>
2024-01-05 17:00:56 +0700

    feat: added prettier, eslint, shad cn and pnpm

---
+++ prettier.config.js
@@ -0,0 +1,37 @@
+/** @type {import('prettier').Config} */
+
+module.exports = {
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^types$",
+    "^@/types/(.*)$",
+    "^@/config/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/hooks/(.*)$",
+    "^@/components/ui/(.*)$",
+    "^@/components/(.*)$",
+    "^@/registry/(.*)$",
+    "^@/app/(.*)$",
+    "",
+    "^(?!.*[.]css$)[./].*$",
+    ".css$",
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+}
---
+++ src/components/test.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+
+const Test = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Test: {count}</h1>
+      <Button onClick={() => setCount(count + 1)}>Increment</Button>
+    </div>
+  );
+};
+
+export { Test };
